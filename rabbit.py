ebbfcaa67966589964ac2b991342c81f09db8eda
Catherine Devlin <catherine.devlin@gmail.com>
2018-08-08 19:00:14 -0400

    knight on screen

--- rabbit.py
+++ rabbit.py
@@ -1,8 +1,16 @@
+"""
+
+Sprite thanks:
+https://opengameart.org/content/knight-sprite
+
+"""
+
 import arcade
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+SPRITE_SCALING_PLAYER = 1.0
 
 class MyGame(arcade.Window):
     """ Main application class. """
@@ -14,12 +22,25 @@ class MyGame(arcade.Window):
 
     def setup(self):
         # Set up your game here
-        pass
+
+        self.player_list = arcade.SpriteList()
+        self.monster_list = arcade.SpriteList()
+
+        # Score
+        self.score = 0
+
+        # Set up the player
+        # Character image from kenney.nl
+        self.player_sprite = arcade.Sprite("knight.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 400  # Starting position
+        self.player_sprite.center_y = 400
+        self.player_list.append(self.player_sprite)
 
     def on_draw(self):
         """ Render the screen. """
         arcade.start_render()
         # Your drawing code goes here
+        self.player_list.draw()
 
     def update(self, delta_time):
         """ All the logic to move, and the game logic goes here. """
