01f75039dee9d7f9d1a4bcd739eeeb4305798854
Catherine Devlin <catherine.devlin@gmail.com>
2018-08-07 22:44:45 -0400

    blank video game for trivia

--- trivia.py
+++ trivia.py
@@ -21,41 +21,59 @@ python -m arcade.examples.drawing_primitives
 import arcade
 import os
 
-# Set the working directory (where we expect to find files) to the same
-# directory this .py file is in. You can leave this out of your own
-# code, but it is needed to easily run the examples using "python -m"
-# as mentioned at the top of this program.
-file_path = os.path.dirname(os.path.abspath(__file__))
-os.chdir(file_path)
+class Knight(arcade.Sprite):
 
-# Open the window. Set the window title and dimensions (width and height)
-arcade.open_window(600, 600, "Drawing Example")
+    def __init__(self, filename, scale, name):
 
-# Set the background color to white
-# For a list of named colors see
-# http://arcade.academy/arcade.color.html
-# Colors can also be specified in (red, green, blue) format and
-# (red, green, blue, alpha) format.
-arcade.set_background_color(arcade.color.LIGHT_GRAY)
+        self.name = name
+        return super().__init__(filename, scale)
 
-# Start the render process. This must be done before any drawing commands.
-arcade.start_render()
 
-# Draw a filled in polygon
-point_list = ((0, 400), (0, 0), (150, 0), (100, 400), )
-arcade.draw_polygon_filled(point_list, arcade.color.GREEN)
-point_list = ((600, 400), (600, 0), (450, 0), (500, 400), )
-arcade.draw_polygon_filled(point_list, arcade.color.GREEN)
 
-# Draw a filled in circle
-# arcade.draw_text("draw_circle_filled", 363, 207, arcade.color.BLACK, 10)
-for x in range(115, 490, 22):
-    arcade.draw_circle_filled(x, 390, 10, arcade.color.BROWN)
+class MyGame(arcade.Window):
 
-# Finish the render.
-# Nothing will be drawn without this.
-# Must happen after all draw commands
-arcade.finish_render()
+    def __init__(self):
 
-# Keep the window up until someone closes it.
-arcade.run()
+        super().__init__(600, 600)
+
+        self.game_over = False
+
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
+
+    def draw_scenery(self):
+
+        arcade.set_background_color(arcade.color.LIGHT_GRAY)
+
+        # Start the render process. This must be done before any drawing commands.
+        arcade.start_render()
+
+        # Draw a filled in polygon
+        point_list = ((0, 400), (0, 0), (150, 0), (100, 400), )
+        arcade.draw_polygon_filled(point_list, arcade.color.GREEN)
+        point_list = ((600, 400), (600, 0), (450, 0), (500, 400), )
+        arcade.draw_polygon_filled(point_list, arcade.color.GREEN)
+
+        # Draw a filled in circle
+        # arcade.draw_text("draw_circle_filled", 363, 207, arcade.color.BLACK, 10)
+        for x in range(115, 490, 22):
+            arcade.draw_circle_filled(x, 390, 10, arcade.color.BROWN)
+
+        # Finish the render.
+        # Nothing will be drawn without this.
+        # Must happen after all draw commands
+
+
+    def start_new_game(self):
+
+        self.draw_scenery()
+
+
+def main():
+    window = MyGame()
+    window.start_new_game()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
+
